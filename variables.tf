767a536d02086fa033c5fed59d404434d729ace2
Priya <vishnuasadi2@gmail.com>
2026-08-16 22:37:17 +0530

    Add Akamai Terraform configuration

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "edgerc_path" {
+  type    = string
+  default = "C:\\Users\\lsaty\\.edgerc"
+}
+
+variable "config_section" {
+  type    = string
+  default = "default"
+}
+
+variable "contract_id" {
+  type    = string
+  default = "ctr_V-3YSNQK2"
+}
+
+variable "group_id" {
+  type    = string
+  default = "grp_211815"
+}
+
+variable "activate_latest_on_staging" {
+  type    = bool
+  default = false
+}
+
+variable "activate_latest_on_production" {
+  type    = bool
+  default = false
+}
